f3c70593db553cb4ecb260c4887d7678c0c0a87a
Sourajit Karada <sourajitkarada@gmail.com>
2019-04-16 15:19:47 +0530

    Changed value of Q based on screen height
    Changed value of Q based on screen height

--- src/components/Cropper/Cropper.constants.js
+++ src/components/Cropper/Cropper.constants.js
@@ -1,7 +1,22 @@
-import { Dimensions } from 'react-native';
-
+import { Dimensions, Platform, PixelRatio } from 'react-native';
 export const SCREEN_WIDTH = Dimensions.get('window').width;
 export const SCREEN_HEIGHT = Dimensions.get('window').height;
-export const Q = 160; // buttons container height
+const scale = SCREEN_HEIGHT / 480;
+function normalize(size) {
+    const newSize = size * scale
+    if (Platform.OS === 'ios') {
+      return Math.round(PixelRatio.roundToNearestPixel(newSize));
+    } else {
+      return Math.round(PixelRatio.roundToNearestPixel(newSize)) - 2;
+    }
+  }
+let normalizedvalue=190;
+if(SCREEN_HEIGHT > 540 ) {
+    normalizedvalue = normalize(140);
+} else {
+    normalizedvalue = normalize(160);
+}
+
+export const Q = normalizedvalue; // buttons container height
 export const H = SCREEN_HEIGHT - Q;
 export const W = SCREEN_WIDTH;
